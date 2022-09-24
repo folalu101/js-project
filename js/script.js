8e51353af58e451dac1ea0ad4182464f521ae30e
A.Adefolalu B <adefolalucode101@gmail.com>
2022-09-24 15:51:58 +0100

    add function to count sentence

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// business logic
+
+
+
+// let wordCounter = " how are you doing my dear friend, i miss you so much for 12 years and 75 days. ";
+
+// count total number of word in a sentence
+function wordCounter(text){
+    
+    if (text.trim().length === 0){
+        return 0;
+    }
+    // console.log(word);
+    let wordCount = 0;
+    const wordArray = text.split(" ");
+    wordArray.forEach(function(words){
+        if (!Number(words)){
+            wordCount ++;
+        }
+        return wordCounter;
+    });
+}
+
+// count total number of sentence in a passage
+
+function sentenceCounter(passage){
+    if (passage.trim().length === 0){
+        return 0;
+    }
+    let sentenceCount = 0;
+    const sentenceArray = passage.split(" ");
+    sentenceArray.forEach(function(passages){
+        if (!Number(passages)){
+            sentenceCount ++;
+        }
+        return sentenceCounter;
+    });
+    
+}
