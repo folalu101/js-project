44ad1f8397ec94d8e4d18f11d1e3e21dde195f55
A.Adefolalu B <adefolalucode101@gmail.com>
2022-10-08 11:27:44 +0100

    write function for no inputed word inbusiness logic

--- js/script.js
+++ js/script.js
@@ -2,38 +2,53 @@
 
 
 
-// let wordCounter = " how are you doing my dear friend, i miss you so much for 12 years and 75 days. ";
+// // let wordCounter = " how are you doing my dear friend, i miss you so much for 12 years and 75 days. ";
 
-// count total number of word in a sentence
-function wordCounter(text){
+// // count total number of word in a sentence
+// function wordCounter(text){
     
-    if (text.trim().length === 0){
-        return 0;
-    }
-    // console.log(word);
-    let wordCount = 0;
-    const wordArray = text.split(" ");
-    wordArray.forEach(function(words){
-        if (!Number(words)){
-            wordCount ++;
-        }
-        return wordCounter;
-    });
+//     if (text.trim().length === 0){
+//         return 0;
+//     }
+//     // console.log(word);
+//     let wordCount = 0;
+//     const wordArray = text.split(" ");
+//     wordArray.forEach(function(words){
+//         if (!Number(words)){
+//             wordCount ++;
+//         }
+//         return wordCounter;
+//     });
+// }
+
+// // count total number of sentence in a passage
+
+// function sentenceCounter(passage){
+//     if (passage.trim().length === 0){
+//         return 0;
+//     }
+//     let sentenceCount = 0;
+//     const sentenceArray = passage.split(" ");
+//     sentenceArray.forEach(function(passages){
+//         if (!Number(passages)){
+//             sentenceCount ++;
+//         }
+//         return sentenceCounter;
+//     });
+    
+// }
+
+// business logic
+function noInputtedWord(word, text){
+    ((word.trim().length === 0) || (text.trim().length === 0))
+    return 0;
 }
 
-// count total number of sentence in a passage
+// word counter
+function wordCounter(text){
+    if(text.trim().length === 0){
 
-function sentenceCounter(passage){
-    if (passage.trim().length === 0){
-        return 0;
     }
-    let sentenceCount = 0;
-    const sentenceArray = passage.split(" ");
-    sentenceArray.forEach(function(passages){
-        if (!Number(passages)){
-            sentenceCount ++;
-        }
-        return sentenceCounter;
-    });
+    let wordCount = 0;
     
 }
